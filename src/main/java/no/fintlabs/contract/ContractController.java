f2cc786aaacf38a372d77aedea60efdb8c865a47
Sander Nilsen Rysjedal <Sander.Rysjedal@vigoiks.no>
2025-05-12 14:01:26 +0200

    add get by id endpoint

--- src/main/java/no/fintlabs/contract/ContractController.java
+++ src/main/java/no/fintlabs/contract/ContractController.java
@@ -1,20 +1,32 @@
 package no.fintlabs.contract;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RequestMapping("/contract")
+@Slf4j
 @RestController
+@RequestMapping("/contract")
 public class ContractController {
 
     private ContractService contractService;
 
     public ContractController(ContractService contractService) {
         this.contractService = contractService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AdapterContractEntity> getContract(@PathVariable String id) {
+        log.info("");
+        if (contractService.exists(id)) {
+            return ResponseEntity.ok(contractService.getContract(id));
+        }
+        else return ResponseEntity.notFound().build();
     }
 
 
